9ff68b91e2d7512dc0b8b6fcc1d35ec7ca52d579
breeg554 <dvdk98@gmail.com>
2024-03-31 10:31:01 +0200

    Create cron expression error class

--- src/utils/CronExpression.ts
+++ src/utils/CronExpression.ts
@@ -1,12 +1,14 @@
+import { InvalidCronExpression } from './errors';
+
 // @todo handle better validation
 const MINUTE_PATTERN = /^(?:\*|[0-5]?\d)$/;
 const HOUR_PATTERN = /^(?:\*|[01]?\d|2[0-3])$/;
 const MONTH_PATTERN =
   /^(?:\*|0?[1-9]|1[0-2]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)$/;
 const DAY_OF_WEEK_PATTERN =
-  /^(?:\*|[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT|[0-6]L|[0-6]#[1-5])$/;
+  /^(?:\*|[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT|[0-6](?:,\d{2})*(?:\/\d+)?|#[1-5])$/;
 const DAY_OF_MONTH_PATTERN =
-  /^(?:\*|0?[1-9]|[12]\d|3[01]|[1-9]?[0-9]W?|LW?|W)$/;
+  /^(?:\*|0?[1-9]|[12]\d|3[01]|[0-9]{2}(?:,\d{2})*(?:\/\d+)?|LW?|W)$/;
 
 type CronTouple = [string, string, string, string, string];
 
@@ -32,19 +34,21 @@ export class CronExpression {
     },
   ) {
     if (!CronExpression.isValid(expression)) {
-      throw new Error(`Invalid cron expression: ${expression}`);
+      throw new InvalidCronExpression(expression);
     }
 
     const parts = [...expression.split(' ')];
 
-    if (args?.minutes) parts[0] = args.minutes;
-    if (args?.hours) parts[1] = args.hours;
-    if (args?.dayOfMonth) parts[2] = args.dayOfMonth;
-    if (args?.month) parts[3] = args.month;
-    if (args?.dayOfWeek) parts[4] = args.dayOfWeek;
+    if (args) {
+      parts[0] = args.minutes ?? parts[0];
+      parts[1] = args.hours ?? parts[1];
+      parts[2] = args.dayOfMonth ?? parts[2];
+      parts[3] = args.month ?? parts[3];
+      parts[4] = args.dayOfWeek ?? parts[4];
+    }
 
     if (!CronExpression.isValid(parts.join(' '))) {
-      throw new Error(`Invalid cron expression: ${parts.join(' ')}`);
+      throw new InvalidCronExpression(parts.join(' '));
     }
 
     return new CronExpression(...(parts as CronTouple), args?.offset ?? 0);
@@ -97,14 +101,15 @@ export class CronExpression {
     const parts = [...expression.split(' ')];
 
     if (parts.length != 5) {
-      throw new Error(`Invalid cron length: ${expression}`);
+      throw new InvalidCronExpression(expression);
     }
-    if (!MINUTE_PATTERN.test(parts[0])) return false;
-    if (!HOUR_PATTERN.test(parts[1])) return false;
-    if (!DAY_OF_MONTH_PATTERN.test(parts[2])) return false;
-    if (!MONTH_PATTERN.test(parts[3])) return false;
-    if (!DAY_OF_WEEK_PATTERN.test(parts[4])) return false;
 
-    return true;
+    return (
+      MINUTE_PATTERN.test(parts[0]) &&
+      HOUR_PATTERN.test(parts[1]) &&
+      DAY_OF_MONTH_PATTERN.test(parts[2]) &&
+      MONTH_PATTERN.test(parts[3]) &&
+      DAY_OF_WEEK_PATTERN.test(parts[4])
+    );
   }
 }
